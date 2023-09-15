1f6e76c3011eeca15e7a1e76c47f1e66c5aa2c89
NathanMorasse <2133133@etu.cchic.ca>
2023-09-15 10:16:02 -0400

    Serveur/ types de messages

---
+++ Code/TicTacToe_Server/TicTacToe_Server/Models/GameStartedMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe_Server.Models
+{
+    public class GameStartedMessage
+    {
+        public string Message { get; set; }
+        public bool IsClientTurn { get; set; }
+
+        public GameStartedMessage(string message, bool isClientTurn)
+        {
+            Message = message;
+            IsClientTurn = isClientTurn;
+        }
+    }
+}
--- Code/TicTacToe_Server/TicTacToe_Server/Models/SocketManager.cs
+++ Code/TicTacToe_Server/TicTacToe_Server/Models/SocketManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using Newtonsoft.Json;
@@ -28,9 +29,10 @@ namespace TicTacToe_Server.Models
                 socket.Listen(10);
                 handler = socket.Accept();
 
-                string confirmationMessage = "Connexion établie";
-                byte[] confirmationBytes = Encoding.UTF8.GetBytes(confirmationMessage);
-                handler.Send(confirmationBytes);
+                Message message = new Message("Connexion réussie");
+                string jsonMessage = JsonConvert.SerializeObject(message);
+                byte[] messageBytes = Encoding.UTF8.GetBytes(jsonMessage+"<EOF>");
+                handler.Send(messageBytes);
 
                 //Connected need to update page
             }
@@ -42,19 +44,47 @@ namespace TicTacToe_Server.Models
 
         public static void SendMove(Move move)
         {
-            string jsonMove = JsonConvert.SerializeObject(move);
-            byte[] moveBytes = Encoding.UTF8.GetBytes(jsonMove);
+            Message moveMessage = new Message("Move", move);
+            string jsonMove = JsonConvert.SerializeObject(moveMessage);
+            byte[] moveBytes = Encoding.UTF8.GetBytes(jsonMove + "<EOF>");
             handler.Send(moveBytes);
         }
 
         public static void NotifyClientNewGame(bool isClientTurn)
         {
-
+            Message message = new Message("GameStarted", isClientTurn);
+            string jsonMessage = JsonConvert.SerializeObject(message);
+            byte[] messageBytes = Encoding.UTF8.GetBytes(jsonMessage + "<EOF>");
+            handler.Send(messageBytes);
         }
 
-        public static void WaitForOpponentMove()
+        public static void WaitForOpponentMessage()
         {
+            string data = null;
+            byte[] bytes = null;
 
+            while (true)
+            {
+                bytes = new byte[1024];
+                int bytesRec = handler.Receive(bytes);
+                data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+                string response = data.Trim();
+
+                if (response != null)
+                {
+                    break;
+                }
+            }
+            Message messageRecu = JsonConvert.DeserializeObject<Message>(data);
+
+            switch (messageRecu.Type)
+            {
+                case "Move":
+                    Game.ValidateMove(messageRecu.MoveMessage);
+                    break;
+                default:
+                    break;
+            }
         }
 
         public static void SendInvalidMoveMessage()
@@ -63,6 +93,11 @@ namespace TicTacToe_Server.Models
         }
 
         public static void ValidateOpponentWin()
+        {
+
+        }
+
+        public static void SendTieMessage()
         {
 
         }
