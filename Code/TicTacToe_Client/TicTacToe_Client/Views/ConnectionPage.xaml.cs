1e96d70dd49848e330ad2d7f3b6247af4b71b028
BachirHassan <2130301@etu.cchic.ca>
2023-09-17 16:17:54 -0400

    Ajout des gestions d'erreur

--- Code/TicTacToe_Client/TicTacToe_Client/Views/ConnectionPage.xaml.cs
+++ Code/TicTacToe_Client/TicTacToe_Client/Views/ConnectionPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TicTacToe_Client.Models;
 
 namespace TicTacToe_Client.Views
 {
@@ -27,7 +28,32 @@ namespace TicTacToe_Client.Views
 
         private void Connect_Click(object sender, RoutedEventArgs e)
         {
+            int Port_Number = 0;
+            if(!int.TryParse(Port_Input.Text, out Port_Number))
+            {
+                MessageBox.Show("Le port saisi est invalide", "Port Invalide", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                try
+                {
+                    SocketManager.ConnectToServer(IP_Input.Text, Port_Number);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message, ex.GetType().ToString(), MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+                
+        }
 
+        private void EnableConnect(object sender, TextChangedEventArgs e)
+        {
+            if ((IP_Input.Text != null && !string.IsNullOrEmpty(IP_Input.Text))
+                && (Port_Input.Text != null && !string.IsNullOrEmpty(Port_Input.Text)))
+            {
+                Connect.IsEnabled = true;
+            }
         }
     }
 }
